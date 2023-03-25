caea165f237f6e831ec85b0f9538fb2cc2a60cd6
marynvdl <maryn@laarse.co.za>
2023-03-25 15:54:10 +0200

    Created basic button to toggle basemap

---
+++ frontend/naturewatch/src/components/BasemapButtonComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+/** Props */
+// export interface Props {
+//   basemaptype: string;
+// }
+
+// const props = withDefaults(defineProps<Props>(), {
+//   basemaptype: 'satellite',
+// });
+
+/** Emits */
+const emit = defineEmits<{
+  (e: 'changeBaseMap', value: string): void;
+}>();
+</script>
+
+<template>
+  <div>
+    <v-btn variant="tonal" @click="emit('changeBaseMap', 'street')">
+      Street
+    </v-btn>
+  </div>
+</template>
+
+<style scoped></style>
--- frontend/naturewatch/src/components/MapComponent.vue
+++ frontend/naturewatch/src/components/MapComponent.vue
@@ -1,26 +1,51 @@
 <script setup lang="ts">
-import {
-  onMounted
-} from 'vue';
+import { onMounted, ref } from 'vue';
 import * as mapboxgl from 'mapbox-gl';
+import BasemapButtonComponent from '@/components/BasemapButtonComponent.vue';
 
+/** Interfaces */
+interface MapboxMap {
+  accessToken: string;
+  container: string;
+  style: string;
+  center: [number, number];
+  zoom: number;
+}
 
 /** Props */
 defineProps<{
   msg: string;
 }>();
 
+/** Data */
+const map = ref<mapboxgl.Map | null>(null);
+
+const mapOptions: MapboxMap = {
+  accessToken:
+    'pk.eyJ1Ijoic29jaWFsZXhwbG9yZXIiLCJhIjoiREFQbXBISSJ9.dwFTwfSaWsHvktHrRtpydQ',
+  container: 'mapDiv',
+  style: 'mapbox://styles/mapbox/satellite-streets-v12?optimize=true',
+  center: [20.23928, 7.35074],
+  zoom: 5,
+};
 
 onMounted(() => {
-    new mapboxgl.Map({
-      accessToken: "pk.eyJ1Ijoic29jaWFsZXhwbG9yZXIiLCJhIjoiREFQbXBISSJ9.dwFTwfSaWsHvktHrRtpydQ",
-      container: "mapDiv",
-      style: "mapbox://styles/mapbox/satellite-streets-v12?optimize=true",
-      center: [20.23928, 7.35074],
-      zoom: 5,
-    });
+  map.value = new mapboxgl.Map(mapOptions);
 });
 
+/** Methods */
+function changeBaseMap() {
+  if (map.value !== null) {
+    const currentStyle = map.value.getStyle().name;
+    if (currentStyle === 'Mapbox Satellite Streets') {
+      map.value.setStyle('mapbox://styles/mapbox/dark-v11');
+    } else {
+      map.value.setStyle(
+        'mapbox://styles/mapbox/satellite-streets-v12?optimize=true'
+      );
+    }
+  }
+}
 </script>
 
 <template>
@@ -35,8 +60,8 @@ onMounted(() => {
       /> -->
       <!-- <p>{{ msg }}</p> -->
 
-      <div id="mapDiv"></div>
-      
+      <div id="mapDiv" />
+      <BasemapButtonComponent @change-base-map="changeBaseMap" />
     </v-responsive>
   </v-container>
 </template>
@@ -50,6 +75,4 @@ onMounted(() => {
 #mapDiv {
   height: 100vh;
 }
-
-
 </style>
