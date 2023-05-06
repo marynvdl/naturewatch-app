fab40fb0ca778e11f23f760b40d7267474c7f4e3
marynvdl <maryn@laarse.co.za>
2023-05-06 10:47:35 +0100

    Revert "Trying to fix CORS errors"
    This reverts commit 2a6f1af55de85bf7f49c7a3200cc201d63bbe942.
    
    Revert wrong commit "Trying to fix CORS errors"

--- frontend/naturewatch/src/store/BasemapStore.ts
+++ frontend/naturewatch/src/store/BasemapStore.ts
@@ -1,49 +1,34 @@
 import { ref, type Ref } from 'vue';
 import { defineStore } from 'pinia';
 import type Basemap from '@/interfaces/BasemapInterface';
-import type {
-  BasemapSourceType,
-  BasemapCustomType,
-} from '../types/BasemapTypes';
 
 /** Basemap Store */
 const useBasemapStore = defineStore('basemap', () => {
   // State
-
-  /** Initial state */
-  const title: Ref<string> = ref('Streets');
-  const source: Ref<BasemapSourceType> = ref('mapbox');
-  const url: Ref<string> = ref('mapbox://styles/mapbox/streets-v12');
-  const customType: Ref<BasemapCustomType> = ref('raster');
+  const title: Ref<string> = ref('Satellite');
+  const url: Ref<string> = ref(
+    'mapbox://styles/mapbox/satellite-streets-v12?optimize=true'
+  );
 
   // Getters
-
-  /** Return current basemap */
+  /**
+   *
+   */
   function currentBasemap(): Basemap {
-    return {
-      title: title.value,
-      source: source.value,
-      url: url.value,
-      ...(customType.value ? { customType: customType.value } : {}),
-    };
+    return { title: title.value, url: url.value };
   }
 
   // Actions
-
-  /** Toggle basemap between satellite and streets */
+  /**
+   *
+   */
   function toggleBasemap() {
     if (title.value === 'Satellite') {
       title.value = 'Streets';
-      source.value = 'mapbox';
       url.value = 'mapbox://styles/mapbox/streets-v12';
     } else {
       title.value = 'Satellite';
-      source.value = 'custom';
-      // url.value =
-      //   'http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}';
-      url.value =
-        'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg';
-      customType.value = 'raster';
+      url.value = 'mapbox://styles/mapbox/satellite-streets-v12?optimize=true';
     }
   }
 
