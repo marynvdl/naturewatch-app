fd8db5cd29fcf452533715c42db0a063ebe37241
marynvdl <maryn@laarse.co.za>
2023-08-17 08:48:07 +0200

    Remove label when shape is deleted and redraw label upon style load

--- frontend/naturewatch/src/components/MeasureComponent.vue
+++ frontend/naturewatch/src/components/MeasureComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, defineProps, onMounted, watch, onBeforeUnmount } from 'vue';
+import { ref, onMounted, watch, onBeforeUnmount } from 'vue';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import turfLength from '@turf/length';
@@ -13,6 +13,9 @@ const props = defineProps({
 });
 
 const map = ref(props.mapInstance);
+
+// Keep track of stored labels
+const storedLabels = ref<Array<{ id: string, description: string, coordinates: number[] | undefined }>>([]);
 
 onMounted(() => {
   const draw = new MapboxDraw({
@@ -30,6 +33,7 @@ onMounted(() => {
 // Measure with turf
 map.value?.on('draw.create', measure);
 map.value?.on('draw.update', measure);
+map.value?.on('draw.delete', removeMeasurementLabel);
 
 function measure(event: any) {
   const geometry = event.features[0].geometry;
@@ -37,7 +41,7 @@ function measure(event: any) {
   let position;
 
   // Unique ID for the source and layer
-  const id = `measurement-${Date.now()}`;
+  const id = `label-${event.features[0].id}`;
 
   if (geometry.type === 'LineString') {
     const distance = turfLength(geometry);
@@ -77,23 +81,80 @@ function measure(event: any) {
       'text-anchor': 'center',
     },
   });
+
+  // Store the label
+  storedLabels.value.push({
+    id,
+    description: label,
+    coordinates: position?.geometry.coordinates
+});
 }
 
-// Remove enent listeners when component is destroyed
+function removeMeasurementLabel(event: any) {
+    const id = `label-${event.features[0].id}`;
+    if (map.value?.getSource(id)) {
+        map.value.removeLayer(id);
+        map.value.removeSource(id);
+    }
+
+    // Remove the label from storedLabels
+    const index = storedLabels.value.findIndex(label => label.id === id);
+    if (index !== -1) {
+        storedLabels.value.splice(index, 1);
+    }
+}
+
+
+// Remove event listeners when component is destroyed
 const unwatch = watch(map, (newValue, oldValue) => {
   if (oldValue) {
     oldValue.off('draw.create', measure);
     oldValue.off('draw.update', measure);
+    oldValue.off('draw.delete', removeMeasurementLabel);
   }
   if (newValue) {
     newValue.on('draw.create', measure);
     newValue.on('draw.update', measure);
+    newValue.on('draw.delete', removeMeasurementLabel);
   }
 });
+
 
 onBeforeUnmount(() => {
   unwatch();
 });
+
+
+map.value?.on('style.load', () => {
+    // Re-add the labels
+    storedLabels.value.forEach(label => {
+        // Re-add the label
+        map.value?.addSource(label.id, {
+            type: 'geojson',
+            data: {
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: label.coordinates
+                },
+                properties: {
+                    description: label.description,
+                },
+            },
+        });
+
+        map.value?.addLayer({
+            id: label.id,
+            type: 'symbol',
+            source: label.id,
+            layout: {
+                'text-field': ['get', 'description'],
+                'text-anchor': 'center',
+            },
+        });
+    });
+});
+
 
 
 </script>
