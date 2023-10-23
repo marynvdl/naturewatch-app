54e8e0a3486e5fb735c526b16e95e1a6b7c25904
marynvdl <maryn@laarse.co.za>
2023-10-23 14:16:57 +0200

    Remove font-awesome package as not used

--- frontend/naturewatch/src/main.ts
+++ frontend/naturewatch/src/main.ts
@@ -13,14 +13,6 @@ import vuetify from './plugins/vuetify';
 // Load Layout vue.
 import App from './App.vue';
 
-// Load Font Awesome
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faPhone);
-
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 /** Register Vue */
 const vue = createApp(App);
 vue.use(router);
@@ -29,5 +21,5 @@ vue.use(vuetify);
 
 // Run!
 router.isReady().then(() => {
-  vue.component("font-awesome-icon", FontAwesomeIcon).mount('#app');
+  vue.mount('#app');
 });
