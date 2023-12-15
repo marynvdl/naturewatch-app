bfb569251a00323481d0f0ff638915b8b03843d9
marynvdl <maryn@laarse.co.za>
2023-12-15 10:44:52 +0100

    Add loading screen and fix eslint issue

--- frontend/naturewatch/src/App.vue
+++ frontend/naturewatch/src/App.vue
@@ -104,6 +104,10 @@ onMounted(() => {
   document.title = title;
   loading.value = false;
   setTheme();
+  const preloader = document.getElementById('preloader');
+  if (preloader) {
+    preloader.style.display = 'none';
+  }
 });
 </script>
 
